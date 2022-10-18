230839a5eca7e644b1cd43754b139b6cb0c44fe6
ivad <yvesyvad@gmail.com>
2022-10-18 08:44:05 +0200

    Add mobile designs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_gray: '#f2f2f2',
+        secondary_gray: '#dadce0',
+        primary_dark: '#3c4043',
+        secondary_dark: '#70757a',
+        dark_gray: '#adb2b7',
+        primary_blue: '#4285f4;',
+        primary_dark_blue: '#1a73e8',
+      },
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        xsm: '0.813rem',
+      },
+    },
   },
   plugins: [],
 };
