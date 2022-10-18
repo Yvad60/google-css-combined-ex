99ddcc46e606f923c79888f67f11113176b47731
ivad <yvesyvad@gmail.com>
2022-10-18 12:15:00 +0200

    Add desktop designs

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,17 @@ module.exports = {
         dark_gray: '#adb2b7',
         primary_blue: '#4285f4;',
         primary_dark_blue: '#1a73e8',
+        secondary_dark_blue: '#1a0dab',
+        light_gray: '#f8f9fa',
       },
       fontFamily: {
         Roboto: ['Roboto', 'sans-serif'],
       },
       fontSize: {
         xsm: '0.813rem',
+      },
+      boxShadow: {
+        main: '0 1px 6px rgb(32 33 36 / 28%)',
       },
     },
   },
